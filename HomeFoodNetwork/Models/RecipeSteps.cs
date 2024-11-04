e0173b3f4f9fdb4e53a4485abbcb56f417fdd44b
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2024-11-03 21:59:06 -0800

    Add RecipeSteps class to model recipe steps
    Made a new model to save the instructions for each Recipe made

---
+++ HomeFoodNetwork/Models/RecipeSteps.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HomeFoodNetwork.Models
+{
+    public class RecipeSteps
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Foreign key for the recipe
+        /// </summary>
+        [ForeignKey("Recipe")]
+        public int RecipeId { get; set; }
+
+        /// <summary>
+        /// Order of the step in the recipe
+        /// </summary>
+        public int StepNumber { get; set; }
+
+        /// <summary>
+        /// Description/Detail of the step
+        /// </summary>
+        public string StepDescription { get; set; }
+
+        /// <summary>
+        /// Navigation property for the recipe
+        /// </summary>
+        public Recipe Recipe { get; set; }
+    }
+}
