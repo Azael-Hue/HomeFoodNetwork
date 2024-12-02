fa21f7c8229348d75410abe5f5099ed08b8ea4e3
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2024-12-01 16:45:24 -0800

    Add email sending functionality using SendGrid
    Introduce IEmailProvider interface and implement with SendGrid.
    
    Update HomeController to use IEmailProvider for sending emails.
    
    Register EmailProviderSendGrid in dependency injection container.

---
+++ HomeFoodNetwork/Models/IEmailProvider.cs
@@ -0,0 +1,36 @@
+﻿using SendGrid.Helpers.Mail;
+using SendGrid;
+
+namespace HomeFoodNetwork.Models
+{
+    public interface IEmailProvider
+    {
+        Task SendEmailAsync(string toEmail, string fromEmail, string subject,
+                                string content, string htmlContent);
+    }
+
+    public class EmailProviderSendGrid : IEmailProvider
+    {
+        private readonly IConfiguration _config;
+        public EmailProviderSendGrid(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string fromEmail, string subject, string content, string htmlContent)
+        {
+            var apiKey = _config.GetSection("SendGridKey").Value;
+            var client = new SendGridClient(apiKey);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress("azael0105.s@gmail.com", "Test Email"),
+                Subject = "Testing 123",
+                PlainTextContent = "This is a test Email",
+                HtmlContent = "<strong>This is a test Email</strong>"
+            };
+            msg.AddTo(new EmailAddress("azael0105.s@gmail.com", "Ian Hue"));
+            await client.SendEmailAsync(msg);
+            // var response = await client.SendEmailAsync(msg).ConfigureAwait(false);
+        }
+    }
+}
